930f3e2f02c48dda2b178e607f467ef6e622ac1c
Jagoda Kuczkowska <jagoda.kucz@wp.pl>
2025-04-25 12:37:32 +0200

    pokedex-view styled

---
+++ src/app/core/pokedex-view/components/pokemon-details/pokemon-details.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pokemon-details',
+  imports: [],
+  templateUrl: './pokemon-details.component.html',
+  styleUrl: './pokemon-details.component.scss'
+})
+export class PokemonDetailsComponent {
+
+}
